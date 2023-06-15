921639bf8e845392a466e43a7f3379fcc43b6966
jdyepesc2s <jesus.yepes@c2spr.com>
2023-06-15 11:57:45 -0400

    Initial commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,44 @@
 const path = require('path');
 
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+
+
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env("DATABASE_HOST", "127.0.0.1"),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "db_emasc_test"),
+//       username: env("DATABASE_USERNAME", "postgres"),
+//       password: env("DATABASE_PASSWORD", "1234"),
+//       schema: env("DATABASE_SCHEMA", "public"),
+//     },
+//   }
+// });
+
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'db_name'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'postgres'),
+      ssl: env.bool('DATABASE_SSL', false),
+      schema: env("DATABASE_SCHEMA", "public"),
     },
-    useNullAsDefault: true,
   },
+  pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
 });
