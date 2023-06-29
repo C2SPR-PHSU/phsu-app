5b3b61955c51a80575bf0ac132a7b809fb7c5965
SoujiGitCode <dev.reinaldo.cardenas@gmail.com>
2023-06-29 05:41:42 +0200

    basic routes added

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,26 @@
 import { Route, Routes } from 'react-router-dom';
-import {
-  Home,
-  Login
-} from '@/views';
+import { Home, Login, Register, NotFound } from '@/views';
 import { PATH } from './constants';
 import { useGlobalContext } from "@/contexts/MainContext";
 
 const Root = () => {
-  const { globalState: { activate: isAuthenticated} } = useGlobalContext();
+  const { globalState: { activate: isAuthenticated } } = useGlobalContext();
 
   return (
     <Routes>
-    {
-      !isAuthenticated ? (
-        <Route path={PATH.ROOT} element={<Login />}></Route>
-      ) : (
-        <Route path={PATH.ROOT} element={<Home />}></Route>
-      )
-    }
+      {
+        !isAuthenticated ? (
+          <Route path={PATH.ROOT} element={<Login />}></Route>
+        ) : (
+          <Route path={PATH.ROOT} element={<Home />}></Route>
+        )
+      }
+      <Route path={PATH.LOGIN} element={<Login />}></Route>
+      <Route path={PATH.REGISTER} element={<Register />} />
+      <Route path={PATH.NOT_FOUND} element={<NotFound />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
-  
+
 export default Root;
