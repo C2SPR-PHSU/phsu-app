00c9d2ca72bc9f95c499d2f19de13840c834460a
freelf <liseth.yggdrasil@gmail.com>
2023-06-30 06:01:06 -0400

    feat: basic body Home template

---
+++ src/views/Home/components/Table/index.tsx
@@ -0,0 +1,59 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+function createData(
+  service: string,
+  time: string,
+  status: string,
+  action: string
+) {
+  return { service, time, status, action };
+}
+
+const rows = [
+  createData("Credential Certification", "03/06/2023", "In-Review", ""),
+  createData(
+    "Admissions Compliance Certification",
+    "0/27/2022",
+    "Approved",
+    ""
+  ),
+  createData("Credit Transcript", "08/27/2022", "Approved", ""),
+];
+
+export default function BasicTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">Service</TableCell>
+            <TableCell align="center">Time</TableCell>
+            <TableCell align="center">Status</TableCell>
+            <TableCell align="center">Action</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.service}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.service}
+              </TableCell>
+              <TableCell align="center">{row.time}</TableCell>
+              <TableCell align="center">{row.status}</TableCell>
+              <TableCell align="center">{row.action}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
