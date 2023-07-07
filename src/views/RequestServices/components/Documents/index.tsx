3701e7e56e7597f68b3e206cac40771fbde52d94
freelf <liseth.yggdrasil@gmail.com>
2023-07-07 07:15:39 -0400

    feat: cute icons and sidebar height fix

--- src/views/RequestServices/components/Documents/index.tsx
+++ src/views/RequestServices/components/Documents/index.tsx
@@ -1,5 +1,8 @@
 import { Grid, Box, Typography } from "@mui/material";
-import { ListItem } from "@/components";
+import UploadIcon from '@mui/icons-material/Upload';
+import DeleteIcon from '@mui/icons-material/Delete';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import CheckIcon from '@mui/icons-material/Check';
 
 const Documents = ({ title }: {title: string}) => {
   return (
@@ -9,10 +12,14 @@ const Documents = ({ title }: {title: string}) => {
           <Typography>{title}</Typography>
         </Grid>
         <Grid item xs={2}>
-          <Typography>BUTTONS</Typography>
+          <Box>
+            <UploadIcon sx={{color: "#009999"}} />
+            <DeleteIcon sx={{color: "rgba(0, 168, 168, 0.42)"}} />
+            <VisibilityIcon sx={{color: "rgba(0, 168, 168, 0.42)"}} />
+          </Box>
         </Grid>
         <Grid item xs={2}>
-          <Typography>CHECK</Typography>
+          <CheckIcon sx={{ color: '#f7941d', textAlign: 'center'}} />
         </Grid>
       </Grid>
     </>
