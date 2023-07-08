16d8dac89a3202398c0cb7ac49d07b0d8863aaf0
davidM20 <smd18t@outlook.com>
2023-07-08 00:22:29 -0400

    registration validation completed

--- src/views/Register/index.tsx
+++ src/views/Register/index.tsx
@@ -41,34 +41,72 @@ export default function Registration() {
     validationSchema: Yup.object({
       firstName: Yup.string()
         .matches(/^[A-Za-z]+$/, "First Name should only contain letters")
-        .required("First Name is required"),
+        .required("First Name is required")
+        .max(20, "First Name must be at most 20 characters"),
 
       lastName: Yup.string()
         .matches(/^[A-Za-z]+$/, "Last Name should only contain letters")
-        .required("Last Name is required"),
-      secondLastName: Yup.string(),
+        .required("Last Name is required")
+        .max(20, "Last Name must be at most 20 characters"),
+
+      secondLastName: Yup.string().max(
+        20,
+        "Second Last Name must be at most 20 characters"
+      ),
+
       email: Yup.string()
         .email("Invalid email address")
-        .required("Email is required"),
+        .required("Email is required")
+        .max(20, "Email must be at most 20 characters"),
 
-      phoneNumber: Yup.string().required("Cell Phone is required"),
-      studentId: Yup.string().required("Student ID is required"),
-      middleName: Yup.string().required("Middle Name is required"),
-      birthdate: Yup.string().required("Birthdate is required"),
-      addressLine1: Yup.string().required("Address Line 1 is required"),
-      addressLine2: Yup.string().required("Address Line 2 is required"),
-      addressState: Yup.string().required("Address State is required"),
-      addressCity: Yup.string().required("Address City is required"),
+      phoneNumber: Yup.string()
+        .required("Cell Phone is required")
+        .matches(/^[0-9]+$/, "Cell Phone should only contain numbers")
+        .max(20, "Cell Phone must be at most 20 characters"),
+
+      studentId: Yup.string()
+        .required("Student ID is required")
+        .max(20, "Student ID must be at most 20 characters"),
+
+      middleName: Yup.string()
+        .required("Middle Name is required")
+        .max(20, "Middle Name must be at most 20 characters"),
+
+      birthdate: Yup.string()
+        .required("Birthdate is required")
+        .max(20, "Birthdate must be at most 20 characters"),
+
+      addressLine1: Yup.string()
+        .required("Address Line 1 is required")
+        .max(20, "Address Line 1 must be at most 20 characters"),
+
+      addressLine2: Yup.string()
+        .required("Address Line 2 is required")
+        .max(20, "Address Line 2 must be at most 20 characters"),
+
+      addressState: Yup.string()
+        .required("Address State is required")
+        .max(20, "Address State must be at most 20 characters"),
+
+      addressCity: Yup.string()
+        .required("Address City is required")
+        .max(20, "Address City must be at most 20 characters"),
+
       addressZipcode: Yup.number().required("Address Zip Code is required"),
+
       password: Yup.string()
         .required("Password is required")
         .min(8, "Password must be at least 8 characters long")
-        .oneOf([Yup.ref("repeatPassword")], "The passwords do not match"),
+        .oneOf([Yup.ref("repeatPassword")], "The passwords do not match")
+        .max(20, "Password must be at most 20 characters"),
+
       repeatPassword: Yup.string()
         .required("Password is required")
         .min(8, "Password must be at least 8 characters long")
-        .oneOf([Yup.ref("password")], "The passwords do not match"),
+        .oneOf([Yup.ref("password")], "The passwords do not match")
+        .max(20, "Password must be at most 20 characters"),
     }),
+
     onSubmit: (values) => {
       try {
         const response = requestRegister({
