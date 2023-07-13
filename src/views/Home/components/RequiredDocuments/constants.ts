7b37dc3e89c1b0cf12be6d904324d4ab8717c525
davidM20 <smd18t@outlook.com>
2023-07-13 14:28:59 -0400

    segundo intento

--- src/views/Home/components/RequiredDocuments/constants.ts
+++ src/views/Home/components/RequiredDocuments/constants.ts
@@ -1,10 +1,3 @@
-export const tableHeaders = [
-  { id: 'table-header-1', title: 'Required Documents' },
-  { id: 'table-header-2', title: 'Expiration date' },
-  { id: 'table-header-3', title: 'Status' },
-  { id: 'table-header-4', title: 'Action' }
-]
-
 export const modalStyle = {
   position: 'absolute' as 'absolute',
   top: '50%',
