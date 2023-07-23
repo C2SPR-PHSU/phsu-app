7ce6770102685bffc8846d0173c55bfa3cc6efa8
davidM20 <smd18t@outlook.com>
2023-07-23 13:09:59 -0400

    yessss, Now just a little touch-up of color is missing

--- src/views/Home/components/RequieredDocumentsTableMobile/index.tsx
+++ src/views/Home/components/RequieredDocumentsTableMobile/index.tsx
@@ -1,13 +1,12 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import {
+  Box,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableHead,
   TableRow,
-  Paper,
-  Box,
   Typography,
 } from "@mui/material";
 import { tableHeaders } from "../RequiredDocumentsTable/constants";
@@ -24,13 +23,6 @@ interface RequiredDocumentsTableProps {
 const RequiredDocumentsTableMobile: React.FC<RequiredDocumentsTableProps> = ({
   documentList,
 }) => {
-  // reference
-  const myheight = useRef<HTMLDivElement | null>(null);
-  const myheightCell = useRef<HTMLDivElement | null>(null);
-
-  const [minHeightValue, setMinHeightValue] = useState(100);
-  const [minHeightValueCell, setMinHeightValueCell] = useState(100);
-
   const [openModal, setOpenModal] = useState(false);
   const [modalMessage, setModalMessage] = useState("");
 
@@ -43,16 +35,6 @@ const RequiredDocumentsTableMobile: React.FC<RequiredDocumentsTableProps> = ({
     return formattedDate;
   }
 
-  useEffect(() => {
-    // Verificar si la referencia está disponible y calcular la altura real
-    if (myheight.current) {
-      setMinHeightValue(myheight.current.offsetHeight);
-    }
-    if (myheightCell.current) {
-      setMinHeightValueCell(myheightCell.current.offsetHeight);
-    }
-  }, [myheight, myheightCell]);
-
   // <----------------------------- View Mobile ------------------------------------>
 
   const displayModal = (message: string) => {
@@ -64,46 +46,76 @@ const RequiredDocumentsTableMobile: React.FC<RequiredDocumentsTableProps> = ({
     <TableContainer sx={{ display: "flex", flexDirection: "row" }}>
       <TableContainer>
         <Table aria-label="simple table">
-          <TableHead>
+          <TableHead sx={{ height: "10vh" }}>
             <TableRow>
               {tableHeaders?.map((header) => {
                 if (header.title !== "Action") {
                   return (
                     <TableCell align="center" key={header.id}>
-                      <div ref={myheight}>{header.title}</div>
+                      {header.title}
                     </TableCell>
                   );
                 }
+                return null;
               })}
             </TableRow>
           </TableHead>
           <TableBody>
             {documentList.length ? (
-              documentList?.map((row, index) => {
+              documentList?.map((row) => {
                 return (
-                  <>
-                    <TableRow
-                      key={index}
-                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                    >
-                      <TableCell component="th" scope="row" ref={myheightCell}>
+                  <TableRow
+                    key={row.id}
+                    sx={{
+                      "&:last-child td, &:last-child th": { border: 0 },
+                      borderTopLeftRadius: "5px",
+                      borderBottomLeftRadius: "5px",
+                    }}
+                  >
+                    <TableCell component="th" scope="row">
+                      <Box
+                        key={row.id}
+                        sx={{
+                          display: "flex",
+                          flexDirection: "column",
+                          justifyContent: "center",
+                          overflowY: "auto",
+                          minHeight: "4.5rem",
+                          maxHeight: "4.5rem",
+                          minWidth: "12rem",
+                          scrollbarWidth: "none",
+                          "&::-webkit-scrollbar": {
+                            width: "0.4em",
+                          },
+                          "&::-webkit-scrollbar-thumb": {
+                            backgroundColor: "transparent",
+                          },
+                        }}
+                      >
                         <Typography>{row.description}</Typography>
-                      </TableCell>
+                      </Box>
+                    </TableCell>
 
-                      <TableCell align="center">
-                        {formatDate(row.created)}
-                      </TableCell>
-                      <TableCell align="center">
-                        <StatusButton statusName={row.status_desc as string} />
-                      </TableCell>
-                    </TableRow>
-                  </>
+                    <TableCell align="center">
+                      {formatDate(row.created)}
+                    </TableCell>
+                    <TableCell
+                      align="center"
+                      sx={{
+                        padding: "0rem",
+                      }}
+                    >
+                      <StatusButton statusName={row.status_desc as string} />
+                    </TableCell>
+                  </TableRow>
                 );
               })
             ) : (
               <TableRow>
                 <TableCell align="center" scope="row">
-                  <Typography textAlign="center">No content here</Typography>
+                  <Typography textAlign="center">
+                    No hay contenido aquí
+                  </Typography>
                 </TableCell>
               </TableRow>
             )}
@@ -117,83 +129,71 @@ const RequiredDocumentsTableMobile: React.FC<RequiredDocumentsTableProps> = ({
       </TableContainer>
       <TableContainer sx={{ width: "15vh" }}>
         <Table aria-label="simple table">
-          <TableHead>
+          <TableHead sx={{ height: "10vh" }}>
             <TableRow>
-              {tableHeaders?.map((header) => {
-                if (header.title === "Action") {
-                  return (
-                    <TableCell align="center" key={header.id}>
-                      <div
-                        style={{
-                          minHeight: minHeightValue,
-                        }}
-                      >
-                        {header.title}
-                      </div>
-                    </TableCell>
-                  );
-                }
-              })}
+              <TableCell align="center">Action</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {documentList.length ? (
               documentList?.map((row, index) => {
                 return (
-                  <>
-                    <TableRow
-                      key={index}
-                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                    >
-                      {/* actions */}
-                      <TableCell align="center">
-                        <div
-                          style={{
-                            minHeight: minHeightValueCell,
-                          }}
-                        >
-                          {row.url ? (
-                            <a
-                              href={row.url}
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              <VisibilityIcon
-                                sx={{ color: "#009999", cursor: "pointer" }}
-                              />
-                            </a>
-                          ) : (
+                  <TableRow
+                    key={index}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    {/* actions */}
+                    <TableCell align="center">
+                      <Box
+                        sx={{
+                          display: "flex",
+                          flexDirection: "row",
+                          alignItems: "center",
+                          justifyContent: "center",
+                          overflowY: "auto",
+                          minHeight: "4.5rem",
+                          maxHeight: "4.5rem",
+                        }}
+                      >
+                        {row.url ? (
+                          <a
+                            href={row.url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
                             <VisibilityIcon
-                              sx={{
-                                color: "#009999",
-                                cursor: "default",
-                                opacity: 0.5,
-                              }}
+                              sx={{ color: "#009999", cursor: "pointer" }}
                             />
-                          )}
-                          {row.ob_message && (
-                            <ChatIcon
-                              sx={{
-                                fontSize: "1.4rem",
-                                color: "#f7941d",
-                                cursor: "pointer",
-                                marginLeft: "0.5rem !important",
-                              }}
-                              onClick={() => displayModal(row.ob_message)}
-                            />
-                          )}
-                          {myheightCell.current?.offsetHeight}
-                        </div>
-                      </TableCell>
-                    </TableRow>
-                  </>
+                          </a>
+                        ) : (
+                          <VisibilityIcon
+                            sx={{
+                              color: "#009999",
+                              cursor: "default",
+                              opacity: 0.5,
+                            }}
+                          />
+                        )}
+
+                        {row.ob_message && (
+                          <ChatIcon
+                            sx={{
+                              fontSize: "1.4rem",
+                              color: "#f7941d",
+                              cursor: "pointer",
+                              marginLeft: "0.5rem !important",
+                            }}
+                            onClick={() => displayModal(row.ob_message)}
+                          />
+                        )}
+                      </Box>
+                    </TableCell>
+                  </TableRow>
                 );
               })
             ) : (
               <TableRow>
-                <TableCell align="center" scope="row">
-                  <Typography textAlign="center">No content here</Typography>
-                </TableCell>
+                <TableCell align="center" scope="row"></TableCell>
               </TableRow>
             )}
           </TableBody>
