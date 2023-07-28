28e337132e4cf43320b17f59345405a78e1ee977
SoujiGitCode <dev.reinaldo.cardenas@gmail.com>
2023-07-28 18:56:44 +0200

    recovery modal added

--- src/views/Recovery/index.tsx
+++ src/views/Recovery/index.tsx
@@ -1,13 +1,25 @@
-import { Grid, Box, Typography, TextField, Button } from "@mui/material";
+import { Grid, Box, Typography, TextField, Button, useTheme, useMediaQuery } from "@mui/material";
 import styles from "./styles.module.scss";
+
+import { Dialog, DialogContent, DialogContentText, DialogTitle, DialogActions } from "@mui/material";
+
 
 import CustomLabel from "@/components/CustomLabel";
 import { useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import ApiRequest from "@/utils/services/apiService";
+import { useState } from "react";
 
 const Recovery = () => {
+  // Estados para manejar el modal
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState('');
+  const [title, setTitle] = useState('');
+
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+
   const navigate = useNavigate();
   const formik = useFormik({
     initialValues: {
@@ -20,23 +32,36 @@ const Recovery = () => {
         .max(100, "Email must be at most 100 characters"),
     }),
 
-    onSubmit: (values) => {
+    onSubmit: async (values) => {
       const recoveryRequest = {
         email: values.email,
       };
 
       const api = new ApiRequest();
-      api.resource = "/recovery";
+      api.resource = "/user/recoverypwd";
       try {
-        const response = api.post({
+        const response = await api.post({
           body: recoveryRequest,
         });
-        console.log(response);
-        navigate("/");
+
+        // Verificamos el código de respuesta
+        if (response.code === 200) {
+          // Abrimos el modal
+          setOpen(true);
+          setTitle('Password recovery email sent');
+          setMessage('Please check your email inbox');
+
+        }
+
+
       } catch (error) {
+        setOpen(true);
+        setTitle('Password recovery');
+        setMessage('Email not registered in the Portal, please verify and enter the correct information');
         console.error(error);
       }
     },
+
   });
 
   // Button Cancel
@@ -132,6 +157,28 @@ const Recovery = () => {
             </Box>
           </Box>
         </Grid>
+
+
+        <Dialog
+          fullScreen={fullScreen}
+          open={open}
+          onClose={() => setOpen(false)}
+          aria-labelledby="responsive-dialog-title"
+        >
+          <DialogTitle id="responsive-dialog-title">
+            {title}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              {message}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button autoFocus onClick={() => setOpen(false)}>
+              Cancel
+            </Button>
+          </DialogActions>
+        </Dialog>
       </Grid>
     </>
   );
