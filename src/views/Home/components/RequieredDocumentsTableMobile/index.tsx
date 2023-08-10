c37486fe977091a0c591193de2ead296d78ade8a
davidM20 <smd18t@outlook.com>
2023-08-10 13:39:33 -0400

    refactor

--- src/views/Home/components/RequieredDocumentsTableMobile/index.tsx
+++ src/views/Home/components/RequieredDocumentsTableMobile/index.tsx
@@ -26,22 +26,24 @@ const RequiredDocumentsTableMobile: React.FC<RequiredDocumentsTableProps> = ({
   const [openModal, setOpenModal] = useState(false);
   const [modalMessage, setModalMessage] = useState("");
 
+  // Function to format a date string
   function formatDate(inputDate: string) {
     const date = new Date(inputDate);
-    const day = date.getDate().toString().padStart(2, "0");
-    const month = (date.getMonth() + 1).toString().padStart(2, "0");
-    const year = date.getFullYear().toString();
-    const formattedDate = `${day}/${month}/${year}`;
+    const formattedDate = `${date.getDate().toString().padStart(2, "0")}/${(
+      date.getMonth() + 1
+    )
+      .toString()
+      .padStart(2, "0")}/${date.getFullYear().toString()}`;
     return formattedDate;
   }
-  // Box
-  const refArray = useRef<(HTMLDivElement | null)[]>([]);
 
-  // TableRow
+  // Refs for storing heights of table cells
+  const refArray = useRef<(HTMLDivElement | null)[]>([]);
   const rowRefArray = useRef<(HTMLTableRowElement | null)[]>([]);
 
+  // State variables for heights
   const [heights, setHeights] = useState<number[]>([]);
-  const [rowheights, setRowHeights] = useState<number[]>([]);
+  const [rowHeights, setRowHeights] = useState<number[]>([]);
 
   // Calculate and set heights on document list change
   useEffect(() => {
@@ -67,8 +69,7 @@ const RequiredDocumentsTableMobile: React.FC<RequiredDocumentsTableProps> = ({
     setRowHeights(newRowHeights);
   }, [documentList]);
 
-  // <----------------------------- View Mobile ------------------------------------>
-
+  // Display modal with a message
   const displayModal = (message: string) => {
     setOpenModal(true);
     setModalMessage(message);
@@ -80,85 +81,72 @@ const RequiredDocumentsTableMobile: React.FC<RequiredDocumentsTableProps> = ({
         <Table aria-label="simple table">
           <TableHead sx={{ height: "10vh" }}>
             <TableRow>
-              {tableHeaders?.map((header) => {
-                if (header.title !== "Action") {
-                  return (
+              {tableHeaders?.map(
+                (header) =>
+                  header.title !== "Action" && (
                     <TableCell align="center" key={header.id}>
                       {header.title}
                     </TableCell>
-                  );
-                }
-                return null;
-              })}
+                  )
+              )}
             </TableRow>
           </TableHead>
           <TableBody>
             {documentList.length ? (
-              documentList?.map((row, index) => {
-                return (
-                  <TableRow
-                    key={row.id}
+              documentList?.map((row, index) => (
+                <TableRow
+                  key={row.id}
+                  sx={{
+                    "&:last-child td, &:last-child th": { border: 0 },
+                    borderTopLeftRadius: "5px",
+                    borderBottomLeftRadius: "5px",
+                  }}
+                  ref={(e) => (rowRefArray.current[index] = e)}
+                >
+                  <TableCell
+                    component="th"
+                    scope="row"
                     sx={{
-                      "&:last-child td, &:last-child th": { border: 0 },
-                      borderTopLeftRadius: "5px",
-                      borderBottomLeftRadius: "5px",
+                      paddingTop: "0.5rem",
+                      paddingBottom: "0.5rem",
                     }}
-                    ref={(e) => (rowRefArray.current[index] = e)}
                   >
-                    <TableCell
-                      component="th"
-                      scope="row"
+                    <Box
+                      ref={(el: HTMLDivElement) =>
+                        (refArray.current[index] = el)
+                      }
                       sx={{
-                        paddingTop: "0.5rem",
-                        paddingBottom: "0.5rem",
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        overflowY: "auto",
+                        minWidth: "4rem",
+                        backgroundColor: "#e9e8e8",
+                        paddingLeft: "1rem",
+                        borderBottomLeftRadius: "10px",
+                        borderTopLeftRadius: "10px",
+                        paddingRight: "0rem",
+                        width: "220%",
                       }}
                     >
-                      <Box
-                        ref={(el: HTMLDivElement) =>
-                          (refArray.current[index] = el)
-                        }
-                        sx={{
-                          display: "flex",
-                          flexDirection: "column",
-                          justifyContent: "center",
-                          overflowY: "auto",
-
-                          minWidth: "4rem",
-                          scrollbarWidth: "none",
-                          "&::-webkit-scrollbar": {
-                            width: "0.4em",
-                          },
-                          "&::-webkit-scrollbar-thumb": {
-                            backgroundColor: "transparent",
-                          },
-                          backgroundColor: "#e9e8e8",
-                          paddingLeft: "1rem",
-                          borderBottomLeftRadius: "10px",
-                          borderTopLeftRadius: "10px",
-                          paddingRight: "0rem",
-                          width: "220%",
-                        }}
-                      >
-                        <Typography sx={{ width: "11rem", padding: "0.5rem" }}>
-                          {row.description}
-                        </Typography>
-                      </Box>
-                    </TableCell>
-
-                    <TableCell>
-                      <Typography>{formatDate(row.created)}</Typography>
-                    </TableCell>
-                    <TableCell
-                      align="center"
-                      sx={{
-                        padding: "0rem",
-                      }}
-                    >
-                      <StatusButton statusName={row.status_desc as string} />
-                    </TableCell>
-                  </TableRow>
-                );
-              })
+                      <Typography sx={{ width: "11rem", padding: "0.5rem" }}>
+                        {row.description}
+                      </Typography>
+                    </Box>
+                  </TableCell>
+                  <TableCell>
+                    <Typography>{formatDate(row.created)}</Typography>
+                  </TableCell>
+                  <TableCell
+                    align="center"
+                    sx={{
+                      padding: "0rem",
+                    }}
+                  >
+                    <StatusButton statusName={row.status_desc as string} />
+                  </TableCell>
+                </TableRow>
+              ))
             ) : (
               <TableRow>
                 <TableCell align="center" scope="row">
@@ -185,81 +173,77 @@ const RequiredDocumentsTableMobile: React.FC<RequiredDocumentsTableProps> = ({
           </TableHead>
           <TableBody>
             {documentList.length ? (
-              documentList?.map((row, index) => {
-                return (
-                  <TableRow
-                    key={index}
+              documentList?.map((row, index) => (
+                <TableRow
+                  key={index}
+                  sx={{
+                    "&:last-child td, &:last-child th": { border: 0 },
+                    minHeight: `${rowHeights[index]}px`,
+                    maxHeight: `${rowHeights[index]}px`,
+                    display: "flex",
+                    flexDirection: "row",
+                  }}
+                >
+                  <TableCell
                     sx={{
-                      "&:last-child td, &:last-child th": { border: 0 },
-                      minHeight: `${rowheights[index]}px`,
-                      maxHeight: `${rowheights[index]}px`,
+                      paddingLeft: 0,
+                      paddingTop: "0.5rem",
+                      paddingBottom: "0.5rem",
+                      width: "5rem",
                       display: "flex",
-                      flexDirection: "row",
+                      alignItems: "center",
                     }}
                   >
-                    {/* actions */}
-                    <TableCell
+                    <Box
                       sx={{
-                        paddingLeft: 0,
-                        paddingTop: "0.5rem",
-                        paddingBottom: "0.5rem",
-                        width: "5rem",
                         display: "flex",
+                        flexDirection: "row",
                         alignItems: "center",
+                        justifyContent: "center",
+                        overflowY: "auto",
+                        backgroundColor: "#e9e8e8",
+                        width: "100%",
+                        borderTopRightRadius: "10px",
+                        borderBottomRightRadius: "10px",
+                        minHeight: `${heights[index]}px`,
+                        maxHeight: `${heights[index]}px`,
+                        minWidth: "2rem",
                       }}
                     >
-                      <Box
-                        sx={{
-                          display: "flex",
-                          flexDirection: "row",
-                          alignItems: "center",
-                          justifyContent: "center",
-                          overflowY: "auto",
-                          backgroundColor: "#e9e8e8",
-                          width: "100%",
-                          borderTopRightRadius: "10px",
-                          borderBottomRightRadius: "10px",
-                          minHeight: `${heights[index]}px`,
-                          maxHeight: `${heights[index]}px`,
-                          minWidth: "2rem",
-                        }}
-                      >
-                        {row.url ? (
-                          <a
-                            href={row.url}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            <VisibilityIcon
-                              sx={{ color: "#009999", cursor: "pointer" }}
-                            />
-                          </a>
-                        ) : (
+                      {row.url ? (
+                        <a
+                          href={row.url}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
                           <VisibilityIcon
-                            sx={{
-                              color: "#009999",
-                              cursor: "default",
-                              opacity: 0.5,
-                            }}
+                            sx={{ color: "#009999", cursor: "pointer" }}
                           />
-                        )}
-
-                        {row.ob_message && (
-                          <ChatIcon
-                            sx={{
-                              fontSize: "1.4rem",
-                              color: "#f7941d",
-                              cursor: "pointer",
-                              marginLeft: "0.5rem !important",
-                            }}
-                            onClick={() => displayModal(row.ob_message)}
-                          />
-                        )}
-                      </Box>
-                    </TableCell>
-                  </TableRow>
-                );
-              })
+                        </a>
+                      ) : (
+                        <VisibilityIcon
+                          sx={{
+                            color: "#009999",
+                            cursor: "default",
+                            opacity: 0.5,
+                          }}
+                        />
+                      )}
+                      {row.ob_message && (
+                        <ChatIcon
+                          sx={{
+                            fontSize: "1.4rem",
+                            color: "#f7941d",
+                            cursor: "pointer",
+                            marginLeft: "0.5rem !important",
+                          }}
+                          onClick={() => displayModal(row.ob_message)}
+                        />
+                      )}
+                    </Box>
+                  </TableCell>
+                </TableRow>
+              ))
             ) : (
               <TableRow>
                 <TableCell align="center" scope="row"></TableCell>
